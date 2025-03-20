734400e0f1b79cb05d288d4f4b5050b7db290368
ooliv-agentur <info@ooliv.de>
2025-03-20 15:53:32 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/spezli/",  // 👈 Set this to match your GitHub repo name
   server: {
     host: "::",
     port: 8080,
